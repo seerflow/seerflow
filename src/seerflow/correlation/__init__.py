e33450f219b3214e154bfc5259e3573a11c336bb
Fernando Flores <ferjflores@gmail.com>
2026-03-30 16:11:38 -0600

    feat(correlation): export Watermark from correlation module

--- src/seerflow/correlation/__init__.py
+++ src/seerflow/correlation/__init__.py
@@ -1,5 +1,6 @@
 """Cross-source correlation: entity graph, temporal windows, rules."""
 
+from seerflow.correlation.watermark import Watermark
 from seerflow.correlation.window import EntityWindowBuffer
 
-__all__ = ["EntityWindowBuffer"]
+__all__ = ["EntityWindowBuffer", "Watermark"]
