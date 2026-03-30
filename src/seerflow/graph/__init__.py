4ad8da95243d0b80d1eaa1f450763d2482c6af0c
Fernando Flores <ferjflores@gmail.com>
2026-03-30 00:59:32 -0600

    feat(graph): finalize exports for graph module

--- src/seerflow/graph/__init__.py
+++ src/seerflow/graph/__init__.py
@@ -1,5 +1,11 @@
 """Entity relationship graph for cross-source correlation."""
 
+from seerflow.graph.edges import EDGE_TYPE_MAP, EdgeRecord, infer_edges
 from seerflow.graph.entity_graph import EntityGraph
 
-__all__ = ["EntityGraph"]
+__all__ = [
+    "EDGE_TYPE_MAP",
+    "EdgeRecord",
+    "EntityGraph",
+    "infer_edges",
+]
