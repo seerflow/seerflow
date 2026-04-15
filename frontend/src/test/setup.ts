85a82e12d95c684a469050954ca509ac0fc85ef5
Fernando Flores <ferjflores@gmail.com>
2026-04-14 17:10:25 -0700

    feat(S-059): TimeRangeChips + SourceSelect components

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,18 @@
 import "@testing-library/jest-dom/vitest";
+
+// Radix UI primitives use PointerEvent APIs that jsdom does not implement.
+// Polyfill the no-op shape expected by Radix (used by Select, Dialog, etc.).
+if (typeof Element !== "undefined") {
+  if (!Element.prototype.hasPointerCapture) {
+    Element.prototype.hasPointerCapture = () => false;
+  }
+  if (!Element.prototype.setPointerCapture) {
+    Element.prototype.setPointerCapture = () => undefined;
+  }
+  if (!Element.prototype.releasePointerCapture) {
+    Element.prototype.releasePointerCapture = () => undefined;
+  }
+  if (!Element.prototype.scrollIntoView) {
+    Element.prototype.scrollIntoView = () => undefined;
+  }
+}
